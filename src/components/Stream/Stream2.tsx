bf25ea6e762e79319d9fddb586d346d9cd08eafe
Danil <danil.saykin.1999@mail.ru>
2023-10-28 22:35:48 +0500

    del iframe

--- src/components/Stream/Stream2.tsx
+++ src/components/Stream/Stream2.tsx
@@ -6,7 +6,7 @@ export function Stream2() {
             <div className={'StreamBlock'}>
                 {/*<h3 className="mt-5">Live Streaming</h3>*/}
                 {/*<img src="{{ url_for('video_feed') }}"/>*/}
-                <iframe width="500" height="300" src="https://sirius-online.ru/embedded/karas37"></iframe>
+
             </div>
 
 
